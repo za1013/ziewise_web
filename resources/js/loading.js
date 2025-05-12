4780f8598a1c9cabc5b328f85c4ca45153657742
za1013 <za1013@naver.com>
2025-05-12 09:26:37 +0900

    updated loading

--- resources/js/loading.js
+++ resources/js/loading.js
@@ -3,4 +3,7 @@ const loading_video_box = document.getElementById("loading_video_box");
 
 video.addEventListener("ended", (e) => {
   loading_video_box.classList.add("close_video");
+  setTimeout(() => {
+    loading_video_box.style.display = "none";
+  }, 1000);
 });
